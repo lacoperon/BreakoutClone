d4624d25998bdcf35d7a1f25a08a0036c0d94e3b
Elliot Williams <elliot.james.williams@gmail.com>
2015-12-28 22:46:57 -0400

    Added Fonts, Downloaded Music
    http://freesound.org/people/ValentinSosnitskiy/sounds/331596/ <<< Music
    URL for Attribution, if it comes to that

---
+++ Breakout/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	//Level load method
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for level " + name);
+		Application.LoadLevel (name);
+	}
+	
+	//Game quit method
+	public void QuitRequest() {
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
