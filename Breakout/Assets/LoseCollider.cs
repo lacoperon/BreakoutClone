b74fbd9edb19c975dcf5dab66f5c636cbf26db7e
Elliot Williams <elliot.james.williams@gmail.com>
2015-12-31 16:12:57 -0400

    Added New Background, Game Ball
    Added New Background, Game Ball

---
+++ Breakout/Assets/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelmanager;
+	
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print ("Trigger");
+		levelmanager.LoadLevel("Win");
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		print ("Collision");
+	}
+}
---
+++ Breakout/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	void Awake () {
+		//gameObject REFERS TO THE MusicPlayer parent GameObject (and the MusicPlayer parent attributes)
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player destroyed!");
+		}
+		else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
